036bf5538ecf41de4638d51e05659c224e720e21
Valdeva Crema <valdeva_crema@harvard.edu>
2024-01-09 09:02:42 -0500

    Sets feature flag as variable (#29)
    * feature flag fix
    
    * flake8

--- scripts/invoke-task.py
+++ scripts/invoke-task.py
@@ -6,12 +6,7 @@ app1.config_from_object('celeryconfig')
 
 arguments = {"pqid": "0197490463",
              "object_urn": "URN-3:HUL.DRS.OBJECT:101115924",
-             "integration_test": True,
-             "feature_flags": {
-                'dash_feature_flag': "off",
-                'alma_feature_flag': "off",
-                'send_to_drs_feature_flag': "off",
-                'drs_holding_record_feature_flag': "on"}}
+             "integration_test": True}
 
 res = app1.send_task('etd-alma-drs-holding-service.tasks.add_holdings',
                      args=[arguments], kwargs={},
