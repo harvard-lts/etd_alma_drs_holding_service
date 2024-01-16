25495a6f447d91a189cdd02032e80bb5ce8fa8bf
Michael Vandermillen <michael_vandermillen@harvard.edu>
2024-01-16 15:23:05 -0500

    use milliseconds

--- etd/drs_holding_by_dropbox.py
+++ etd/drs_holding_by_dropbox.py
@@ -164,11 +164,11 @@ class DRSHoldingByDropbox():
         mongo_record_for_pqid = self.___get_record_from_mongo()
 
         # Start xml record collection output file
-        yyyymmddhhmmss    = get_date_time_stamp('second')
-        xmlCollectionFileName = f'AlmaDRSDark{instance.capitalize()}_{yyyymmddhhmmss}.xml'
+        yyyymmddhhmmssml    = get_date_time_stamp('millisecond')
+        xmlCollectionFileName = f'AlmaDRSDark{instance.capitalize()}_{yyyymmddhhmmssml}.xml'
         xmlCollectionFile = xmlCollectionFileName
         if integration_test:
-            xmlCollectionFile = f'AlmaDRSDarkTest{instance.capitalize()}_{yyyymmddhhmmss}.xml'
+            xmlCollectionFile = f'AlmaDRSDarkTest{instance.capitalize()}_{yyyymmddhhmmssml}.xml'
             schoolMatch = re.match(r'proquest\d+-\d+-(\w+)', ALMA_TEST_BATCH_NAME)
             if schoolMatch:
                 school = schoolMatch.group(1)
